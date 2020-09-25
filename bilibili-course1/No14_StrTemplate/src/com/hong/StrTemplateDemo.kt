0ceb7dc798363e31ca2cf1195e1ecced01e8d7ea
hongwanzhen <hongwanzhen@cmss.chinamobile.com>
2020-09-26 02:11:49 +0800

    modify

--- bilibili-course1/No14_StrTemplate/src/com/hong/StrTemplateDemo.kt
+++ bilibili-course1/No14_StrTemplate/src/com/hong/StrTemplateDemo.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
 	var s = strGenerater("公园");
 	println(s)
 }
-// 字符串模板，${}表示需要替换的内容，猜测三个双引号表示对于相关表达式的支持
+// 字符串模板，${}表示需要替换的内容，三个双引号表示?
 fun strGenerater(palceName: String): String {
 	var templateStr = """我今天去${palceName}玩，${palceName}有${palceName.length}个字。"""
 	return templateStr
